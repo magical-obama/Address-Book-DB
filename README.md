ac46f27d871452bab8845ca89d73b2bf574083b1
magical-obama <maschwaerzler@a1.net>
2022-04-02 17:41:22 +0200

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Address Book Database
+
+# .env File Needed!
+PORT - The Port on which express serves, otherwise :80
+MONGODB_URI - The URI for the MongoDB server without the DB name!
+MONGODB_DB - The Database Name
--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI).then(
+mongoose.connect(process.env.MONGODB_URI + '/' + process.env.MONGODB_DB).then(
     () => {
         console.log('Connected to MongoDB');
     },
